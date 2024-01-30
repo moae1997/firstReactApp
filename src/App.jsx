7c9b2f6bdb8ea69e7bff6d810851422c73c2345a
Amna Abdallah <amnaabdallah@Amnas-MacBook-Air.local>
2024-01-29 19:03:43 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import './App.css'
+import './index.css'
+import { puppyList } from './data'
+import { useState } from 'react';
+
 
 function App() {
+  
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null)
+  
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup);
   return (
     <>
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )} 
+      { 
+        puppies.map((puppy) => {
+        return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+        })
+      }
     </>
   )
 }
